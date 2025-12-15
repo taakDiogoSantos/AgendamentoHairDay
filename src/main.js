8dfe85fe088bbe72e8eb27779b2ea47dfc45f859
taakDiogoSantos <diogo.santos@taak.com.br>
2025-12-15 14:17:28 -0300

    feat: add dependencies and packages

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict"
+
+//importacao da configuracao do dayjs
+import "./libs/dayjs.js"
+
+//css
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
+
+import dayjs from "dayjs"
+
+console.log(dayjs().format("DD/MM - HH:mm"))
